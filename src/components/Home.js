31e920b03499c38c2612b6103ee81a10c1178ef4
Austin Rhoads <austinrrhoads@ymail.com>
2021-08-15 15:00:27 -0500

    added page link

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,16 @@ class Home extends Component{
     render(){
         return(
             <div id="home-div">
+
+                <div id="text">
                 Hello, I'm Austin Rhoads.
                 <br />
                 I'm a full-stack web developer.
+                </div>
+                
+                <div className="page-link">
+                    View my work
+                </div>
                 
             </div>
         )
